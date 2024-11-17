e628f103b8549afae5f58370d87b076c4f20d98a
Rakhee Singh <rakhisingh2811@gmail.com>
2024-11-17 09:07:57 +0530

    Mock data for servers (#7)
    Co-authored-by: rakhee28 <rakhee@strings.ai>

---
+++ src/app/utils/constant.js
@@ -0,0 +1,6 @@
+export const HEADER_TABS = {SERVER_COMPARISON: "Server Comparison", SERVER_METRICS: "Server Metrics"};
+export const MetricsChartMapping = [
+  { metric: "diskUsage", chart: "BAR" },
+  { metric: "memoryUsage", chart: "PIE" },
+  { metric: "cpuUsage", chart: "BAR" },
+];
