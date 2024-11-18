ce99404796400525a950e817c6ac3753584f743b
rakhee28 <rakhee@strings.ai>
2024-11-18 06:59:43 +0530

    adjust tabs contain css according to screen size

--- src/app/features/serverMetrics/index.jsx
+++ src/app/features/serverMetrics/index.jsx
@@ -7,7 +7,7 @@ import { HEADER_TABS } from "../../utils/constant";
 function ServerMetrics() {
   const { activeTab } = useTabs();
   return (
-    <div className="server-metrics-container mt-10 ml-5 p-6 w-2/3 mr-20 border border-slate-200 flex flex-wrap gap-4">
+    <div className="server-metrics-container mt-10 ml-5 p-6 mr-20 border border-slate-200 flex flex-wrap gap-4">
       {activeTab === HEADER_TABS.SERVER_COMPARISON && <ServersComparison />}
       {activeTab === HEADER_TABS.SERVER_METRICS && <ServerMetricEach />}
     </div>
