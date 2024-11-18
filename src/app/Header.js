831ba8449b611c025ef5205546bcae25ccb39423
rakhee28 <rakhee@strings.ai>
2024-11-18 20:38:43 +0530

    added logo for projrct

--- src/app/Header.js
+++ src/app/Header.js
@@ -3,11 +3,14 @@ import React from "react";
 import Tabs from "./designComponent/tabs/Tabs";
 import NotificationIconWithNumber from "./features/notification";
 import { HEADER_TABS } from "./utils/constant";
+import { Logo } from "./assets/icons";
+
 
 function Header() {
   return (
-    <div className="flex flex-row px-6 pt-2 justify-between border-b border-slate-200" >
-      <div className="flex gap-2">
+    <div className="flex flex-row items-center px-6 pt-4 justify-between border-b border-slate-200" >
+      <div className="flex gap-2 items-center">
+        <Logo className="w-8 h-8"/>
         <Tabs tabs={HEADER_TABS} />
       </div>
       <NotificationIconWithNumber />
