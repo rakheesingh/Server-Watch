e33fccfc4f411beb0c9b248e9c1526dec7f20e0c
rakhee28 <rakhee@strings.ai>
2024-11-25 15:01:24 +0530

    eslint error for display name resolved

--- src/app/utils/higherOrderComponent/WithTopComponent.jsx
+++ src/app/utils/higherOrderComponent/WithTopComponent.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 
-function WithTopComponent(WrappedComponenet) {
-  return function ({ children, ...props }) {
+function WithTopComponent(WrappedComponent) {
+  const ComponentWithTop = ({ children, ...props }) => {
     return (
       <div className="relative">
         {children}
-        <WrappedComponenet {...props} />
+        <WrappedComponent {...props} />
       </div>
     );
   };
+
+  // Set the display name for easier debugging
+  ComponentWithTop.displayName = `WithTop(${getDisplayName(WrappedComponent)})`;
+
+  return ComponentWithTop;
+}
+
+// Helper function to get the display name of the wrapped component
+function getDisplayName(WrappedComponent) {
+  return WrappedComponent.displayName || WrappedComponent.name || 'Component';
 }
 
 export default WithTopComponent;
